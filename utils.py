b24de7611fe2e823d6a2e4c8795366fc3a5e05f0
Adryan Victor Oliveira Gomes <adryan.gomes@arapiraca.ufal.br>
2026-07-06 08:37:48 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
 
+def limpar_tela():
+    """Limpa o terminal para uma melhor experiência de usuário."""
+    # 'nt' é para Windows, 'posix' é para Linux/macOS
+    os.system('cls' if os.name == 'nt' else 'clear')
